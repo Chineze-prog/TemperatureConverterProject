380cbe217e9ff78e78c1bb83344fea77ad405e0d
Chineze-prog <chinezeobi.bomi@gmail.com>
2024-01-26 22:44:13 -0600

    updated code

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,5 +1,7 @@
-//import java.awt.event.*;
+import java.awt.event.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 /**
@@ -16,7 +18,8 @@ public class TemperatureConverter{
     private JButton convertFtoCButton;
     private JLabel celsiusLabel;
     private JLabel fahrenheitLabel;
-    
+    private JTextField cTextField;
+    private JTextField fTextField;
     
     /**
      * Constructor for objects of class TemperatureConverter
@@ -40,25 +43,50 @@ public class TemperatureConverter{
         panel = new JPanel();
 
         convertCtoFButton = new JButton("Convert C to F");
+        //adding listener
+        convertCtoFButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt){
+                //convert C to F
+            }
+        });
+
         convertFtoCButton = new JButton("Convert F to C");
+        convertFtoCButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt){
+                //convert F to C
+            }
+        });
 
         celsiusLabel = new JLabel("Celsius:");
+        celsiusLabel.setBounds(10, 20, 80, 25);
         fahrenheitLabel = new JLabel("Fahrenheit:");
+        fahrenheitLabel.setBounds(10, 20, 80, 25);
 
+        cTextField = new JTextField(20);
+        cTextField.setBounds(100, 20, 165, 25);
+        fTextField = new JTextField(20);
+        cTextField.setBounds(100, 20, 165, 25);
+        
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
         panel.setLayout(new GridLayout(0, 1));
         
-        panel.add(convertCtoFButton);
-        panel.add(convertFtoCButton);
         panel.add(celsiusLabel);
+        panel.add(cTextField);
+        panel.add(convertCtoFButton);
+        
         panel.add(fahrenheitLabel);
-
+        panel.add(fTextField);
+        panel.add(convertFtoCButton);
+        
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Temperature Converter by @Chineze-prog");
         frame.pack();
 
         frame.setVisible(true);
+        //done-thank you for using tc by co
+        //String name = JOptionPane.showInputDialog("Enter your name: ")
+        //JOptionPane.showMessageDialog(null, "Hello " + name)
     }
 
     public static void main(String[] args){
